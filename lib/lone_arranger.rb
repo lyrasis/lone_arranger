8956f18e9135d3e74c943425a3abbb7c6dfa7339
Mark Cooper <mark.c.cooper@outlook.com>
2022-08-22 14:47:58 -0700

    Re-org code

--- lib/lone_arranger.rb
+++ lib/lone_arranger.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
+class Group
+  def self.LONE_ARRANGER_GROUP_CODE
+    "lone-arranger"
+  end
+end
+
 class Repository < Sequel::Model(:repository)
   # after_save not used by ASpace
   def after_save
     super
 
+    create_lone_arranger_group
+  end
+
+  def create_lone_arranger_group
     RequestContext.open(repo_id: self.id) do
       Group.create_from_json(
         JSONModel(:group).from_hash(Repository.lone_arranger_group),
@@ -15,11 +25,15 @@ class Repository < Sequel::Model(:repository)
 
   def self.lone_arranger_group
     {
-      group_code: "lone-arranger",
+      group_code: Group.LONE_ARRANGER_GROUP_CODE,
       description: "Lone Arrangers",
-      grants_permissions: [
-        "view_repository",
-      ]
+      grants_permissions: lone_arranger_group_permissions
     }
+  end
+
+  def self.lone_arranger_group_permissions
+    [
+      "view_repository",
+    ]
   end
 end
